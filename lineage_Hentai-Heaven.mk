4091455d463153d078365da37e23ab150024553b
isus203 <128573607+isus203@users.noreply.github.com>
2024-10-06 15:30:39 +0300

    Update and rename lineage_Hentai_Heaven.mk to lineage_Hentai-Heaven.mk

--- lineage_Hentai-Heaven.mk
+++ lineage_Hentai-Heaven.mk
@@ -15,8 +15,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 $(call inherit-product, device/TECNO/Hentai-Heaven/device.mk)
 
 PRODUCT_BRAND := TECNO
-PRODUCT_DEVICE := Hentai_Heaven
-PRODUCT_NAME := lineage_Hentai_Heaven
+PRODUCT_DEVICE := Hentai-Heaven
+PRODUCT_NAME := lineage_Hentai-Heaven
 PRODUCT_MODEL := Tecno Spark 9 Pro
 PRODUCT_MANUFACTURER := Tecno Mobility Limited
 
@@ -25,7 +25,7 @@ TARGET_CORE_GMS := true
 PRODUCT_GMS_CLIENTID_BASE := android-tecno
 
 TARGET_VENDOR := TECNO
-TARGET_VENDOR_PRODUCT_NAME := Hentai_Heaven
+TARGET_VENDOR_PRODUCT_NAME := Hentai-Heaven
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=KH7n-OP \
